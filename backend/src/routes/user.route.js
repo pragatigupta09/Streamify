095105d953a2d84362e2b2cf33d53a0a7004a77a
pragatigupta09 <pragatigp06@gmail.com>
2025-07-06 10:21:30 +0530

    added onboarding with login/signup and friend request features

---
+++ backend/src/routes/user.route.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import { protectRoute } from '../middleware/auth.middleware.js';
+import { getMyFriends, getRecommendedUsers, sendFriendRequest, acceptFriendRequest, getFriendRequests } from '../controllers/user.controller.js';
+const router=express.Router();
+
+//apply auth middleware to all routes
+router.use(protectRoute);
+
+router.get('/', getRecommendedUsers);
+router.get('/friends',getMyFriends);
+
+router.post("/friend-request/:id", sendFriendRequest);
+router.post("/friend-request/:id/accept", acceptFriendRequest);
+
+router.get('/friend-requests', getFriendRequests);
+
+export default router;
